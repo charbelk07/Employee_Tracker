4c6bbeb7acf729ed4fac5ba85b8f1b4417323dec
charbelk07 <125899348+charbelk07@users.noreply.github.com>
2023-07-29 09:45:28 -0700

    schema and Seed data setup

---
+++ seed.sql
@@ -0,0 +1,15 @@
+INSERT INTO department
+    (name)
+VALUES
+    ("Sales"),
+    ("Service"),
+    ("HR"),
+    ("Accounting");
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ("Sales Managaer", 100000, 1),
+    ("Sales Associate", 65000, 1),
+    ("Service Manager", 80000, 2),
+    ("Sercive Manager Associate", 55000, 2),
